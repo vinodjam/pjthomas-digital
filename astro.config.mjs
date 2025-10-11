5fd2b74356025717b4791f0cf11877c1108cf5c6
Vinod J M <vinodjacobmatthew@gmail.com>
2025-10-11 14:47:58 +0530

    Initial Custom changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
-import starlight from "@astrojs/starlight";
-
+// import starlight from "@astrojs/starlight";
+import pagefind from "astro-pagefind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
@@ -13,115 +13,36 @@ export default defineConfig({
   image: {
     domains: ["images.unsplash.com"],
   },
-  // i18n: {
-  //   defaultLocale: "en",
-  //   locales: ["en", "fr"],
-  //   fallback: {
-  //     fr: "en",
-  //   },
-  //   routing: {
-  //     prefixDefaultLocale: false,
-  //   },
-  // },
+  i18n: {
+    defaultLocale: "en", // All urls that don't contain language prefix will be treated as default locale
+    locales: [
+      "en",
+      {
+        path: "ml", // no slashes included
+        codes: ["ml", "ml-IN"],
+      },
+    ],
+  },
   prefetch: true,
   integrations: [
     sitemap({
       i18n: {
         defaultLocale: "en", // All urls that don't contain language prefix will be treated as default locale
-        locales: {
-          en: "en", // The `defaultLocale` value must present in `locales` keys
-          fr: "fr",
-        },
-      },
-    }),
-    starlight({
-      title: "ScrewFast Docs",
-      // https://github.com/withastro/starlight/blob/main/packages/starlight/CHANGELOG.md
-      // If no Astro and Starlight i18n configurations are provided, the built-in default locale is used in Starlight and a matching Astro i18n configuration is generated/used.
-      // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
-      // If only an Astro i18n configuration is provided, the Starlight i18n configuration is updated to match it.
-      // If both an Astro and Starlight i18n configurations are provided, an error is thrown.
-      locales: {
-        root: {
-          label: "English",
-          lang: "en",
-        },
-        de: { label: "Deutsch", lang: "de" },
-        es: { label: "Español", lang: "es" },
-        fa: { label: "Persian", lang: "fa", dir: "rtl" },
-        fr: { label: "Français", lang: "fr" },
-        ja: { label: "日本語", lang: "ja" },
-        "zh-cn": { label: "简体中文", lang: "zh-CN" },
-      },
-      // https://starlight.astro.build/guides/sidebar/
-      sidebar: [
-        {
-          label: "Quick Start Guides",
-          translations: {
-            de: "Schnellstartanleitungen",
-            es: "Guías de Inicio Rápido",
-            fa: "راهنمای شروع سریع",
-            fr: "Guides de Démarrage Rapide",
-            ja: "クイックスタートガイド",
-            "zh-cn": "快速入门指南",
+        locales: [
+          "en",
+          {
+            path: "ml", // no slashes included
+            codes: ["ml", "ml-IN"],
           },
-          autogenerate: { directory: "guides" },
-        },
-        {
-          label: "Tools & Equipment",
-          items: [
-            { label: "Tool Guides", link: "tools/tool-guides/" },
-            { label: "Equipment Care", link: "tools/equipment-care/" },
-          ],
-        },
-        {
-          label: "Construction Services",
-          autogenerate: { directory: "construction" },
-        },
-        {
-          label: "Advanced Topics",
-          autogenerate: { directory: "advanced" },
-        },
-      ],
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/mearashadowfax/ScrewFast",
-        },
-      ],
-      disable404Route: true,
-      customCss: ["./src/assets/styles/starlight.css"],
-      favicon: "/favicon.ico",
-      components: {
-        SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
-        Head: "./src/components/ui/starlight/Head.astro",
-        MobileMenuFooter:
-          "./src/components/ui/starlight/MobileMenuFooter.astro",
-        ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro",
+        ],
       },
-      head: [
-        {
-          tag: "meta",
-          attrs: {
-            property: "og:image",
-            content: "https://screwfast.uk" + "/social.webp",
-          },
-        },
-        {
-          tag: "meta",
-          attrs: {
-            property: "twitter:image",
-            content: "https://screwfast.uk" + "/social.webp",
-          },
-        },
-      ],
     }),
     compressor({
       gzip: false,
       brotli: true,
     }),
     mdx(),
+    pagefind(),
   ],
   experimental: {
     clientPrerender: true,
---
+++ src/utils/ml/navigation.ts
@@ -0,0 +1,21 @@
+// An array of links for navigation bar
+const navBarLinks = [
+  { name: "Chronology", url: "/ml/posts/chronology", icon: "calendar" },
+  { name: "Academia", url: "/ml/posts/academia", icon: "graduationcap" },
+  { name: "Community", url: "/ml/posts/community", icon: "people" },
+  { name: "In Media", url: "/ml/posts/inmedia", icon: "newspaper" },
+  { name: "Resources", url: "/ml/posts/resources", icon: "archive" },
+];
+
+const footerLinks = [
+  { name: "Contact", url: "/ml/contact", icon: "envelope" },
+  { name: "Changelog", url: "/ml/changelog", icon: "book" },
+];
+
+const homePageName = "Dr P J Thomas Memorial";
+
+export default {
+  navBarLinks,
+  footerLinks,
+  homePageName,
+};
